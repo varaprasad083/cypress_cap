06f342dba8fceb428025cb3c0ecb5cb47f386a28
varaprasad083 <varaprasadsaragada083@gmail.com>
2024-09-12 16:36:57 +0530

    final commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,33 +1,22 @@
-const { defineConfig } = require("cypress");
-const getCompareSnapshotsPlugin = require('cypress-image-diff-js/plugin');
-const Mochawesome = require('cypress-mochawesome-reporter/plugin');
-
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    pageLoadTimeout: 120000, // Increase timeout to 120 seconds
-    defaultCommandTimeout: 10000 ,// Increase default command timeout to 10 seconds
-
-    setupNodeEvents(on, config) {
-      Mochawesome(on);
-      return getCompareSnapshotsPlugin(on, config);
-    
-      
-      
-    },
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      reportDir: 'cypress/reports/mochawesome-report',
-      reportFilename: 'report',
-      quiet: true,
-      overwrite: false,
-      html: true,
-      json: true
-    },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: true,
+    html: true,   // Enable HTML report generation
   },
-
-  video: true,
-  screenshotOnRunFailure: true,
-
-  retries:2 ,
-});
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-image-diff-js/plugin')(on,config)
+      require('cypress-mochawesome-reporter/plugin')(on)
+    }
+  },
+  retries:2,
+  pageLoadTimeout: 60000, // increase timeout to 120 seconds
+  viewportWidth: 1036,
+  viewportHeight: 960,
+  video:true,
+  screenshotOnRunFailure:true
+})
