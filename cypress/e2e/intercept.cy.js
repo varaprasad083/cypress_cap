33022553c9b70af6f6a765cc39864707f897e34f
varaprasad083 <varaprasadsaragada083@gmail.com>
2024-09-13 20:00:14 +0530

    final

--- cypress/e2e/intercept.cy.js
+++ cypress/e2e/intercept.cy.js
@@ -1,11 +1,11 @@
-describe('intercept data',()=>{
-    it('moking with intercept',()=>{
-        cy.visit('https://demowebshop.tricentis.com');
-        cy.intercept('GET','/register',{fixture:'loginfix.json'}).as('posts')
-        cy.get('.ico-register').click()
+// describe('intercept data',()=>{
+//     it('moking with intercept',()=>{
+//         cy.visit('https://demowebshop.tricentis.com');
+//         cy.intercept('GET','/register',{fixture:'loginfix.json'}).as('posts')
+//         cy.get('.ico-register').click()
 
-        cy.wait('@posts')
+//         cy.wait('@posts')
        
-    })
+//     })
 
-})
+// })
